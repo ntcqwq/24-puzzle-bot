b0031272b0d8cc9950873d42c31cd2f171bbb878
Noah <59535872+SSnipro@users.noreply.github.com>
2021-03-06 12:54:31 -0500

    File Path Bug Fix

--- config.py
+++ config.py
@@ -10,9 +10,10 @@ def save_config():
     with open(config_file, 'w') as configfile:
         json.dump(CONFIG, configfile, indent=4,ensure_ascii=False)
 
+run_path = os.path.split(os.path.realpath(__file__))[0]
 RUN_PATH = os.getcwd()
 
-config_file = 'tw.json'
+config_file = f'{run_path}/tw.json'
 CONFIG = load_config()
 if not "LifetimeStats" in CONFIG:
     CONFIG["LifetimeStats"] = {}
